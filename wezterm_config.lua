75ac433330f56b6da61949b73a93580a25b0843d
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-05-31 19:49:24 +0800

    添加本地加载wezterm配置代码

--- wezterm_config.lua
+++ wezterm_config.lua
@@ -1,3 +1,9 @@
+-- 加载该文件配置，~/.config/wezterm/wezterm.lua中加上以下代码
+-- package.path = package.path..";D:/MyRepo/dotfiles/?.lua"
+-- local wezterm_config = require 'wezterm_config'
+-- return wezterm_config
+
+
 local wezterm = require 'wezterm';
 
 return {
