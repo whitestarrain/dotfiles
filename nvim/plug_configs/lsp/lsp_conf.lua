ad529b3bc6a732a7545fdd5a508469bc3c805954
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-06-22 19:49:12 +0800

    根据neovim 0.7 调整hover 的 float win

--- nvim/plug_configs/lsp/lsp_conf.lua
+++ nvim/plug_configs/lsp/lsp_conf.lua
@@ -1,30 +1,32 @@
-vim.cmd[[
+vim.cmd([[
   Plug 'neovim/nvim-lspconfig'
 
   " lsp ui美化
   Plug 'tami5/lspsaga.nvim'
-]]
+]])
 
 require("au")["User LoadPluginConfig"] = function()
+	-- icon config
+	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
 
-  -- icon config
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
+	-- handler 配置，添加参数
+	-- help vim.lsp.util.open_floating_preview() -- 读读源码，这部份实现不算难
+	local lsp = vim.lsp
+	-- Global handlers.
+	lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, {
+		border = "single",
+		offset_x = 1,
+		max_width = 100,
+		max_height = 20,
+	})
 
-  -- handler 配置，添加参数
-  local lsp = vim.lsp
-  -- Global handlers.
-  lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, {
-    border = "single",
-    offset_x = 1,
-    width = 100,
-  })
-
-  lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, {
-    border = "single",
-    width = 100,
-  })
+	lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, {
+		border = "single",
+		max_width = 100,
+		max_height = 20,
+	})
 end
