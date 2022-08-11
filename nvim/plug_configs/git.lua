5650bbc9bbcf4f860719019f3b7aef73295cbb13
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-08-12 00:47:13 +0800

    gitsign添加行blame

--- nvim/plug_configs/git.lua
+++ nvim/plug_configs/git.lua
@@ -62,11 +62,11 @@ require("au")["User LoadPluginConfig"] = function()
 			follow_files = true,
 		},
 		attach_to_untracked = true,
-		current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+		current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 		current_line_blame_opts = {
 			virt_text = true,
 			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-			delay = 1000,
+			delay = 600,
 			ignore_whitespace = false,
 		},
 		current_line_blame_formatter_opts = {
