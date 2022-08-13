40fe5abdc63311d52ff6717082378a067183d01b
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2022-08-13 16:50:13 +0800

    添加markdown doc highlight

--- nvim/plug_configs/theme/neosolarized_nvim.lua
+++ nvim/plug_configs/theme/neosolarized_nvim.lua
@@ -20,16 +20,17 @@ require("au")["User LoadPluginConfig"] = function()
 	local groups = cb.groups
 	local styles = cb.styles
 
+	-- line style
 	Color.new("black", "#000000")
 	Group.new("CursorLine", colors.none, colors.base03, styles.NONE, colors.base1)
 	Group.new("CursorLineNr", colors.yellow, colors.none, styles.NONE, colors.base1)
 	Group.new("Visual", colors.none, colors.base03, styles.reverse)
 
+	-- diagnostic style
 	local cError = groups.Error.fg
 	local cInfo = groups.Information.fg
 	local cWarn = groups.Warning.fg
 	local cHint = groups.Hint.fg
-
 	Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark(), styles.NONE)
 	Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
 	Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
@@ -39,6 +40,28 @@ require("au")["User LoadPluginConfig"] = function()
 	Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
 	Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
 
+	-- float window and it's boarder style
 	Group.new("FloatBorder", colors.none, colors.none)
 	Group.new("NormalFloat", colors.none, colors.none)
+
+	-- markdown doc style
+	local cConstant = groups.Constant.fg
+	Group.new("markdownBlockquote", colors.gray)
+	Group.new("markdownBold", colors.yellow)
+	Group.new("markdownBoldItalic", colors.green)
+	Group.new("markdownCode", cConstant)
+	Group.new("markdownCodeBlock", colors.green)
+	Group.new("markdownCodeDelimiter", colors.green)
+	Group.new("markdownHeadingDelimiter", colors.grey, colors.none)
+	Group.new("markdownHeadingRule", colors.purple, colors.none)
+	Group.new("markdownId", colors.purple)
+	Group.new("markdownIdDeclaration", colors.blue)
+	Group.new("markdownIdDelimiter", colors.purple)
+	Group.new("markdownItalic", colors.purple)
+	Group.new("markdownLinkDelimiter", colors.purple)
+	Group.new("markdownLinkText", colors.red)
+	Group.new("markdownListMarker", colors.red)
+	Group.new("markdownOrderedListMarker", colors.red)
+	Group.new("markdownRule", colors.gray)
+	Group.new("markdownUrl", colors.cyan)
 end
