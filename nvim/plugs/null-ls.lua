f6ab615975895aa3d2193e02015643a18c1d9172
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-03-19 19:15:02 +0800

    null-ls关闭autoformat when write

--- nvim/plugs/null-ls.lua
+++ nvim/plugs/null-ls.lua
@@ -57,20 +57,6 @@ au["User LoadPluginConfig"] = function()
   null_ls.setup({
     sources = null_ls_source,
     on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            lsp_formatting(bufnr)
-          end,
-        })
-      end
     end,
   })
-
-  vim.api.nvim_create_user_command("DisableLspFormatting", function()
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-  end, { nargs = 0 })
 end
