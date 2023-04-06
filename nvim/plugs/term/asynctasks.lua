a611452b8eb82fc1e4a9c09c22a7f6404e34096d
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-04-06 22:52:46 +0800

    task配置优化

--- nvim/plugs/term/asynctasks.lua
+++ nvim/plugs/term/asynctasks.lua
@@ -5,7 +5,7 @@ vim.cmd([[
 ]])
 
 vim.g.asyncrun_open = 6
-vim.g.asynctasks_term_pos = "external"
+vim.g.asynctasks_term_pos = "bottom"
 vim.g.asynctasks_extra_config = {
   vim.g.absolute_config_path .. "./others/tasks.ini",
 }
