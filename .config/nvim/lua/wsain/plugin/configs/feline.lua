e9a7f94c6265dadd803a7850bf70bac08cf3e833
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-05-09 00:13:18 +0800

    配置feline，放弃使用
    feline加载为实时加载，lualine加载则为全部加载完后显示。
    :e 一下，会发现feline的statusline会剧烈抖动。

--- .config/nvim/lua/wsain/plugin/configs/feline.lua
+++ .config/nvim/lua/wsain/plugin/configs/feline.lua
@@ -4,14 +4,6 @@ plugin.config = function()
   local feline = require("feline")
   local vi_mode = require("feline.providers.vi_mode")
   local solarized = {
-    base03 = "#002b36",
-    base02 = "#073642",
-    base01 = "#586e75",
-    base00 = "#657b83",
-    base0 = "#839496",
-    base1 = "#93a1a1",
-    base2 = "#eee8d5",
-    base3 = "#fdf6e3",
     fg = "#839496",
     bg = "#073642",
     yellow = "#b58900",
@@ -21,54 +13,182 @@ plugin.config = function()
     violet = "#6c71c4",
     blue = "#268bd2",
     cyan = "#2aa198",
-    green = "#859900",
+    green = "#719e07",
     black = "#3c3836",
     skyblue = "#83a598",
     oceanblue = "#076678",
     gray = "#93a1a1",
+    darkblue = "#073642",
   }
 
-  -- vi mode color configuration
+  local theme = solarized
+
   local modeColors = {
-    ["NORMAL"] = { fg = solarized.base03, bg = solarized.blue },
-    ["COMMAND"] = { fg = solarized.base03, bg = solarized.skyblue },
-    ["INSERT"] = { fg = solarized.base03, bg = solarized.green },
-    ["VISUAL"] = { fg = solarized.base03, bg = solarized.magenta },
-    ["BLOCK"] = { fg = solarized.base03, bg = solarized.magenta },
-    ["REPLACE"] = { fg = solarized.base03, bg = solarized.red },
-    ["INACTIVE"] = { fg = solarized.base0, bg = solarized.base02 },
+    ["NORMAL"] = { fg = theme.bg, bg = theme.blue },
+    ["COMMAND"] = { fg = theme.bg, bg = theme.blue },
+    -- ["COMMAND"] = { fg = theme.bg, bg = theme.skyblue },
+    ["INSERT"] = { fg = theme.bg, bg = theme.green },
+    ["VISUAL"] = { fg = theme.bg, bg = theme.magenta },
+    ["BLOCK"] = { fg = theme.bg, bg = theme.magenta },
+    ["REPLACE"] = { fg = theme.bg, bg = theme.red },
+    ["INACTIVE"] = { fg = theme.fg, bg = theme.darkblue },
   }
+
+  local icons = {
+    locker = "", -- #f023
+    page = "☰", -- 2630
+    line_number = "", -- e0a1
+    connected = "", -- f817
+    dos = "", -- e70f
+    unix = "", -- f17c
+    mac = "", -- f179
+    mathematical_L = "𝑳",
+    vertical_bar = "┃",
+    vertical_bar_thin = "│",
+    left = "",
+    right = "",
+    block = "█",
+    left_filled = "",
+    right_filled = "",
+    slant_left = "",
+    slant_left_thin = "",
+    slant_right = "",
+    slant_right_thin = "",
+    slant_left_2 = "",
+    slant_left_2_thin = "",
+    slant_right_2 = "",
+    slant_right_2_thin = "",
+    left_rounded = "",
+    left_rounded_thin = "",
+    right_rounded = "",
+    right_rounded_thin = "",
+    circle = "●",
+    git_added = "",
+    git_changed = "",
+    git_removed = "",
+  }
+  local function modeHl()
+    local hl = modeColors[vi_mode.get_vim_mode()]
+    if hl == nil then
+      hl = modeColors["NORMAL"]
+    end
+    return hl
+  end
+
+  local function getDiffStatus(type)
+    -- { added = add_count, modified = modified_count, removed = removed_count }
+    local diffStatus = vim.b.gitsigns_status_dict
+    local result = ""
+    if diffStatus == nil then
+      return result
+    end
+    result = diffStatus[type] or result
+    return result
+  end
 
   local components = {
     -- components when buffer is active
     active = {
-      {}, -- left section
+      {
+        {
+          provider = function()
+            return " " .. vi_mode.get_vim_mode() .. " "
+          end,
+          hl = modeHl,
+          right_sep = {
+            str = icons.right_filled,
+            hl = function()
+              local hl = modeColors[vi_mode.get_vim_mode()]
+              if hl == nil then
+                hl = modeColors["NORMAL"]
+              end
+              local bg = theme.bg
+              if vim.b.gitsigns_head ~= nil then
+                bg = theme.gray
+              end
+              return {
+                fg = hl.bg,
+                bg = bg,
+              }
+            end,
+          },
+        },
+        {
+          provider = function()
+            local branch = vim.b.gitsigns_head or ""
+            return " " .. branch .. " "
+          end,
+          hl = {
+            bg = theme.gray,
+            fg = theme.bg,
+          },
+          right_sep = {
+            str = icons.right_filled,
+            hl = {
+              bg = theme.bg,
+              fg = theme.gray,
+            },
+          },
+        },
+        {
+          provider = {
+            name = "file_info",
+            opts = {
+              type = "base-only",
+              file_modified_icon = "[+]",
+            },
+          },
+          left_sep = " ",
+          right_sep = " ",
+        },
+        {
+          provider = function()
+            return icons.git_added .. " " .. getDiffStatus("added") .. " "
+          end,
+          hl = {
+            fg = theme.green,
+          },
+        },
+        {
+          provider = function()
+            return icons.git_changed .. " " .. getDiffStatus("changed") .. " "
+          end,
+          hl = {
+            fg = theme.yellow,
+          },
+        },
+        {
+          provider = function()
+            return icons.git_removed .. " " .. getDiffStatus("removed") .. " "
+          end,
+          hl = {
+            fg = theme.red,
+          },
+        },
+      }, -- left section
       {}, -- mid section
       {}, -- right section
     },
     -- components when buffer is inactive
     inactive = {
-      {}, -- left section
-      {}, -- right section
+      {
+        {},
+      }, -- left section
+      {
+        {
+          provider = icons.block,
+          hl = {
+            bg = theme.fg,
+            fg = theme.bg,
+          },
+        },
+      }, -- right section
     },
   }
 
-  local spliceComponentCount = 1
-  local function getSpliceComponent(left, right, provider)
-    spliceComponentCount = spliceComponentCount + 1
-    return {
-      name = "spliceComponent" .. spliceComponentCount,
-      provider = provider,
-      hl = {
-        bg = right.hl.bg,
-        fg = left.hl.bg,
-      },
-    }
-  end
-
   feline.setup({
-    --[[ theme = solarized,
-    components = components, ]]
+    theme = theme,
+    components = components,
   })
 end
 return plugin
