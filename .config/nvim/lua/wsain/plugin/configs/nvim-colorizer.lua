4bcbd3ec2eaf18623c7484b906feb44bf2a99401
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-05-14 09:07:39 +0800

    lazyload

--- .config/nvim/lua/wsain/plugin/configs/nvim-colorizer.lua
+++ .config/nvim/lua/wsain/plugin/configs/nvim-colorizer.lua
@@ -1,5 +1,6 @@
 local plugin = require("wsain.plugin.template"):new()
 plugin.shortUrl = "norcalli/nvim-colorizer.lua"
+plugin.loadEvent = "VeryLazy"
 plugin.config = function()
   require("colorizer").setup({ "json", "xml", "html", "css", "ps1", "lua", "vim", "yaml" })
 end
