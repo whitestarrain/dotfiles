6f606209c51dcd46a4d6ed0b253262a0e624f947
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-05-31 23:12:52 +0800

    添加debugpy配置

--- .config/nvim/lua/wsain/plugin/configs/nvim-dap.lua
+++ .config/nvim/lua/wsain/plugin/configs/nvim-dap.lua
@@ -91,6 +91,55 @@ local function setupCodelldb()
   dap.configurations.rust = dap.configurations.cpp
 end
 
+local function setupDebugPy()
+  local dap = require("dap")
+  dap.adapters.python = function(cb, config)
+    print(cmdWrap("debugpy-adapter"))
+    if config.request == "attach" then
+      ---@diagnostic disable-next-line: undefined-field
+      local port = (config.connect or config).port
+      ---@diagnostic disable-next-line: undefined-field
+      local host = (config.connect or config).host or "127.0.0.1"
+      cb({
+        type = "server",
+        port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+        host = host,
+        options = {
+          source_filetype = "python",
+        },
+      })
+    else
+      cb({
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = cmdWrap("debugpy-adapter"),
+          args = { "--port", "${port}" },
+        },
+      })
+    end
+  end
+  dap.configurations.python = {
+    {
+      type = "python",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      pythonPath = function()
+        local cwd = vim.fn.getcwd()
+        if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+          return cwd .. "/venv/bin/python"
+        elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+          return cwd .. "/.venv/bin/python"
+        else
+          return "python3"
+        end
+      end,
+      justMyCode = false,
+    },
+  }
+end
+
 plugin.config = function()
   vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
   vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticError", linehl = "", numhl = "" })
@@ -99,6 +148,7 @@ plugin.config = function()
   vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
 
   setupCodelldb()
+  setupDebugPy()
 end
 plugin.globalMappings = {
   {
