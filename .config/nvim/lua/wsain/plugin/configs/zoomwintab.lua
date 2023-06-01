0efcd6a82c13d4e5edf30ae45b38fdd27453cf55
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-06-01 23:56:40 +0800

    map配置优化

--- .config/nvim/lua/wsain/plugin/configs/zoomwintab.lua
+++ .config/nvim/lua/wsain/plugin/configs/zoomwintab.lua
@@ -4,4 +4,8 @@ plugin.loadEvent = "VeryLazy"
 plugin.globalMappings = {
   { "n", "<leader>zz", ":ZoomWinTabToggle<cr>", "zoom toogle" },
 }
+plugin.config = function()
+  -- disable default map
+  vim.cmd("unmap <C-w_o>")
+end
 return plugin
