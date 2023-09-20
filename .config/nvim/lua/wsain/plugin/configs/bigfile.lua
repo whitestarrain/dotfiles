497eb0c195cf4204e4bd217acbeeda5116c54f47
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-09-20 14:11:16 +0800

    大文件检测配置优化

--- .config/nvim/lua/wsain/plugin/configs/bigfile.lua
+++ .config/nvim/lua/wsain/plugin/configs/bigfile.lua
@@ -2,28 +2,55 @@ local plugin = require("wsain.plugin.template"):new()
 
 plugin.shortUrl = "LunarVim/bigfile.nvim"
 plugin.loadEvent = "VeryLazy"
+
+common_pattern = function(max_length, max_line_length, excluede_file_types)
+  return function(bufnr)
+    max_length = max_length or 10000
+    max_line_length = max_line_length or 1000
+    excluede_file_types = excluede_file_types or {}
+
+    local current_file_type = vim.filetype.match({ buf = bufnr })
+    local expand_name = vim.fn.expand("%:e")
+    for _, file_type in ipairs(excluede_file_types) do
+      if current_file_type == file_type then
+        return false
+      end
+      if expand_name == file_type then
+        return false
+      end
+    end
+
+    -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+    local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+    local file_length = #file_contents
+
+    if file_length > max_length then
+      return true
+    end
+
+    for _, line_content in ipairs(file_contents) do
+      if #line_content > max_line_length then
+        return true
+      end
+    end
+  end
+end
+
 plugin.config = function()
   require("bigfile").setup({
     -- detect long python files
-    pattern = function(bufnr, filesize_mib)
-      -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
-      local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
-      local file_length = #file_contents
-      local filetype = vim.filetype.match({ buf = bufnr })
-      if filetype == "markdown" then
-        return false
-      end
-
-      if file_length > 10000 then
-        return true
-      end
-
-      for _, line_content in ipairs(file_contents) do
-        if #line_content > 1000 then
-          return true
-        end
-      end
-    end,
+    filesize = 5, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+    pattern = common_pattern(10000, 1000, { "markdown", "text" }),
+    features = { -- features to disable
+      "indent_blankline",
+      "illuminate",
+      "lsp",
+      "treesitter",
+      "syntax",
+      "matchparen",
+      "vimopts",
+      "filetype",
+    },
   })
 end
 
