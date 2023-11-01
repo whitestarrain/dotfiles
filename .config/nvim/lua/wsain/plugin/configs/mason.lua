4254e3104e619de2946526e7e00ab888f1a7cde8
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2023-11-02 00:24:53 +0800

    add mason registry

--- .config/nvim/lua/wsain/plugin/configs/mason.lua
+++ .config/nvim/lua/wsain/plugin/configs/mason.lua
@@ -2,11 +2,17 @@ local plugin = require("wsain.plugin.template"):new()
 
 plugin.shortUrl = "williamboman/mason.nvim"
 plugin.loadEvent = "VeryLazy"
+plugin.dependencies = {
+  "mason-org/mason-registry",
+}
 plugin.config = function()
   require("mason").setup({
     install_root_dir = vim.g.absolute_config_path .. ".mason",
     ui = {
       border = "single",
+    },
+    registries = {
+      "github:mason-org/mason-registry",
     },
   })
 end
