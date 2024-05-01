b91bd413e45cc054cdaab0f4616e4737d999598c
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-05-01 11:32:40 +0800

    [nvim] add basic mapping

--- nvim/.config/nvim/lua/wsain/base_mappings.lua
+++ nvim/.config/nvim/lua/wsain/base_mappings.lua
@@ -9,11 +9,16 @@ local set_mapping = function(mode, key, action, opts)
   vim.keymap.set(mode, key, action, utils.merge_tb(default_opts, opts))
 end
 
+local put_empty_line = function(put_above)
+  return function()
+    local target_line = vim.fn.line(".") - (put_above and 1 or 0)
+    vim.fn.append(target_line, vim.fn["repeat"]({ "" }, vim.v.count1))
+  end
+end
+
 -- moving
-set_mapping("n", "j", "gj")
-set_mapping("n", "k", "gk")
-set_mapping("v", "j", "gj")
-set_mapping("v", "k", "gk")
+set_mapping({ "n", "x", "v" }, "j", [[v:count == 0 ? 'gj' : 'j']], { expr = true })
+set_mapping({ "n", "x", "v" }, "k", [[v:count == 0 ? 'gk' : 'k']], { expr = true })
 
 -- win resize
 set_mapping("n", "<c-left>", ":vertical resize -1<CR>")
@@ -25,6 +30,31 @@ set_mapping("n", "<c-down>", ":resize -1<CR>")
 set_mapping("n", "o", "A<cr>")
 set_mapping("n", "<", "<<")
 set_mapping("n", ">", ">>")
+set_mapping("n", "gO", put_empty_line(true), { desc = "Put empty line above" })
+set_mapping("n", "go", put_empty_line(false), { desc = "Put empty line below" })
+
+-- copy/paste
+set_mapping({ "n", "x" }, "gy", '"+y', { desc = "Copy to system clipboard" })
+set_mapping("n", "gp", '"+p', { desc = "Paste from system clipboard" })
+-- - Paste in Visual with `P` to not copy selected text (`:h v_P`)
+set_mapping("x", "gp", '"+P', { desc = "Paste from system clipboard" })
+
+-- Reselect latest changed, put, or yanked text
+set_mapping(
+  "n",
+  "gV",
+  '"`[" . strpart(getregtype(), 0, 1) . "`]"',
+  { expr = true, replace_keycodes = false, desc = "Visually select changed text" }
+)
+
+-- Search inside visually highlighted text. Use `silent = false` for it to
+-- make effect immediately.
+set_mapping("x", "g/", "<esc>/\\%V", { silent = false, desc = "Search inside visual selection" })
+
+if vim.fn.has("nvim-0.10") == 0 then
+  set_mapping("x", "*", [[y/\V<C-R>=escape(@", '/\')<CR><CR>]], { desc = "Search forward" })
+  set_mapping("x", "#", [[y?\V<C-R>=escape(@", '?\')<CR><CR>]], { desc = "Search backward" })
+end
 
 -- change buffer
 set_mapping("n", "<M-h>", ":bp<cr>")
