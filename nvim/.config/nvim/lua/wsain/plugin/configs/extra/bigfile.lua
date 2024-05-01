0cf64db5a3ec4af04f03c3e303b796d45e6f31c7
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-05-01 10:51:14 +0800

    [nvim-bigfile] allow big file undo

--- nvim/.config/nvim/lua/wsain/plugin/configs/extra/bigfile.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/extra/bigfile.lua
@@ -44,14 +44,24 @@ local common_pattern = function(max_length, max_line_length, excluede_file_types
 end
 
 plugin.config = function()
+  local my_vimopts_feature = {
+    name = "my_vimopts_feature",
+    disable = function()
+      vim.opt_local.swapfile = false
+      vim.opt_local.foldmethod = "manual"
+      vim.opt_local.undolevels = 10
+      vim.opt_local.undoreload = 0
+      vim.opt_local.list = false
+    end,
+  }
   local features = {
     "indent_blankline",
     "illuminate",
     "lsp",
     "treesitter",
     "syntax",
-    "vimopts",
     "filetype",
+    my_vimopts_feature,
   }
   -- if disable `matchparen` in win, may not be able to view files
   if require("wsain.utils").getOs() == "linux" then
@@ -60,7 +70,11 @@ plugin.config = function()
   require("bigfile").setup({
     -- detect long python files
     filesize = 5, -- size of the file in MiB, the plugin round file sizes to the closest MiB
-    pattern = common_pattern(10000, 1000, { "markdown", "text", "fugitive", "NvimTree", "msnumber", "bash", "sh", "zsh" }),
+    pattern = common_pattern(
+      10000,
+      1000,
+      { "markdown", "text", "fugitive", "NvimTree", "msnumber", "bash", "sh", "zsh" }
+    ),
     features = features,
   })
 end
