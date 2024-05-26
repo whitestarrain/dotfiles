d55f1696f9546306a67d92eb2e952f49224cc951
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-05-26 18:16:53 +0800

    [nvim asynctask] update project config file name

--- nvim/.config/nvim/lua/wsain/plugin/configs/terminal/asynctasks.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/terminal/asynctasks.lua
@@ -8,8 +8,9 @@ plugin.init = function()
   vim.g.asyncrun_open = 8
   vim.g.asynctasks_term_pos = "bottom"
   vim.g.asynctasks_extra_config = {
-    vim.g.absolute_config_path .. "others/tasks.ini",
+    vim.g.absolute_config_path .. "others/asynctasks.ini",
   }
+  vim.g.asynctasks_config_name = '.asynctasks.ini'
 end
 
 return plugin
