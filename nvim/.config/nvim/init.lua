5dc338bf18b1ccd640415602a9ce81a69d85a013
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-07-21 18:55:56 +0800

    [nvim] prepend config path to runtimepath
    adjust profile priority

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,7 @@ vim.g.absolute_config_path = string.sub(
   0,
   string.len(vim.fn.expand("<sfile>:p")) - string.len(vim.fn.expand("<sfile>:t"))
 )
-vim.opt.rtp:append(string.sub(vim.g.absolute_config_path, 1, -2))
+vim.opt.rtp:prepend(string.sub(vim.g.absolute_config_path, 1, -2))
 
 -- basic config
 require("wsain.opts")
