5ac523a67933bdea4f1e227aef5bf92ed7b19325
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-07-26 23:43:27 +0800

    [nvim] disable default markdown ftplugin

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
-vim.opt.shiftwidth = 2
+-- disable default markdown ftplugin
+vim.b.did_ftplugin = true
 
+vim.opt.shiftwidth = 2
 -- can't right shift line which starts with '#' when smartindent is true
 vim.opt.smartindent = false
