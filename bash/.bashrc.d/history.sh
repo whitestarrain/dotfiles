0a8f48845168b9d5a27a37b6bd8bca7e5809328e
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-09-06 23:46:17 +0800

    [bash] support clearing duplicate history command && support multiline command

--- bash/.bashrc.d/history.sh
+++ bash/.bashrc.d/history.sh
@@ -5,7 +5,7 @@ export HISTTIMEFORMAT="[%Y-%m-%d %H:%M:%S] "
 
 # command history limit
 export HISTFILESIZE=10000                            # HISTFILE limit
-export HISTSIZE=1000                                 # shell session history limit
+export HISTSIZE=2000                                 # shell session history limit
 export HISTIGNORE="&:[ ]*:exit:clear:ls:pwd:nvim:sp" # ignore command pattern, separated by :
 
 # history append
@@ -13,4 +13,9 @@ shopt -s histappend
 
 # realtime update history file
 export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+
+# support multiline command
+shopt -s cmdhist
+shopt -s lithist
+HISTTIMEFORMAT='%F %T '
 
