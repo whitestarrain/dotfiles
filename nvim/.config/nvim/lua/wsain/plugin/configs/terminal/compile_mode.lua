481b768002d4cc64ad68cec6e79c00780ad82b27
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-09-06 08:05:05 +0800

    [nvim] update compile-mode plugin

--- nvim/.config/nvim/lua/wsain/plugin/configs/terminal/compile_mode.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/terminal/compile_mode.lua
@@ -7,13 +7,10 @@ plugin.dependencies = {
   { "m00qek/baleia.nvim", tag = "v1.3.0" },
 }
 plugin.config = function()
-  require("compile-mode").setup({
-    no_baleia_support = false,
+  vim.g.compile_mode = {
     default_command = "make -k",
-    same_window_errors = true,
-  })
+  }
 
-  local compile_mode_group = vim.api.nvim_create_augroup("compile_mode_group", { clear = true })
   require("wsain.plugin.whichkey").register({
     { "<leader>c", group = "code" },
     { "<leader>cc", ":Compile<cr>", desc = "compile" },
