c42df7cb06822ba66f58d0b19a42ff897b26fd4a
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-10-10 08:53:31 +0800

    [nvim] add mapping && fix rainbow highlight

--- nvim/.config/nvim/lua/wsain/plugin/configs/private_mapping.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/private_mapping.lua
@@ -2,7 +2,8 @@ local plugin = require("wsain.plugin.template"):new()
 local utils = require("wsain.utils")
 require("wsain.plugin.whichkey").register({
   { "<leader>d", ":bp|bd #<cr>", desc = "delete buffer" },
-  { "<leader>z", group = "+others" },
+  { "<leader>z", group = "+others", mode = "n" },
+  { "<leader>z", group = "+others", mode = "v" },
   { "<leader>zp", ":syntax sync fromstart<cr>", desc = "syntax sync" },
   { "<leader>zl", ":%s/\\v(\\n\\s*){2,}/\\r\\r/<cr> :/jkjk<cr>", desc = "compress blank line" },
   { "<leader>zf", ':echo expand("%:p")<cr>', desc = "show file path" },
@@ -23,6 +24,23 @@ require("wsain.plugin.whichkey").register({
       utils.save_markdown_url_images(true)
     end,
     desc = "download images using proxy",
+  },
+  {
+    "<leader>e",
+    function()
+      local path_separator = "/"
+      if utils.getOs() == "win" then
+        path_separator = "\\"
+      end
+      vim.api.nvim_feedkeys(":e " .. vim.fn.getcwd() .. path_separator, "n", "false")
+    end,
+    desc = "edit file",
+  },
+  {
+    "<leader>zs",
+    [[:s/[^\x00-\xff]\zs\ze\w\|\w\zs\ze[^\x00-\xff]/ /g<CR>]],
+    desc = "wrap english word with space",
+    mode = "v",
   },
 })
 
