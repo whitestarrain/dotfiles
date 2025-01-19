76c4de8ddb38ad22a093ff7f7b9a18564346d254
wsain <55945266+whitestarrain@users.noreply.github.com>
2025-01-19 15:41:53 +0800

    [nvim] update lsp diagnostic sign icon

--- nvim/.config/nvim/lua/wsain/plugin/configs/ide/lsp.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/ide/lsp.lua
@@ -806,7 +806,7 @@ plugin.config = function()
     },
   })
   -- diagnostic sign config
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  local signs = { Error = "", Warn = "", Hint = "", Info = "" }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
