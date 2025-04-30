145a50a8c72e119c827872fbf7fd73327bea05e1
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-04-30 08:36:47 +0800

    [nvim] update deprecated config

--- nvim/.config/nvim/lua/wsain/plugin/configs/ui/feline.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/ui/feline.lua
@@ -123,7 +123,7 @@ plugin.config = function()
   local function getLspClientName()
     local msg = ""
     local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
