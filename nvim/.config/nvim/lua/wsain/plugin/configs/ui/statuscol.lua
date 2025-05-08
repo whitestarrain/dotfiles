37349c61dba6240778358960ba54a63e96264e35
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2025-05-08 08:10:39 +0800

    [nvim] update statuscol config

--- nvim/.config/nvim/lua/wsain/plugin/configs/ui/statuscol.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/ui/statuscol.lua
@@ -18,11 +18,11 @@ plugin.customConfig = function(lsp_mode)
     -- check plugins's "sign_assign_segment" function to figure out the matching mechanism
     segments = {
       {
-        sign = { namespace = { "diagnostic" }, maxwidth = 1, auto = auto_diagnostic_sign },
+        sign = { namespace = { "diagnostic" }, maxwidth = 1, colwidth = 1, auto = auto_diagnostic_sign },
         click = "v:lua.ScSa",
       },
       {
-        sign = { name = { ".*" }, maxwidth = 1, auto = true },
+        sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true },
       },
       {
         text = { builtin.lnumfunc },
@@ -30,7 +30,7 @@ plugin.customConfig = function(lsp_mode)
         click = "v:lua.ScLa",
       },
       {
-        sign = { namespace = { "gitsign" }, name = { "Dap" }, maxwidth = 1, auto = false },
+        sign = { namespace = { "gitsign" }, name = { "Dap" }, maxwidth = 1, colwidth = 1, auto = false },
         click = "v:lua.ScSa",
       },
     },
