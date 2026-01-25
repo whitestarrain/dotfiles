db6552cc52c1f5205e59119c7d274e6df2b2b22d
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-01-25 11:05:38 +0800

    [emacs] show inline image when edit markdown

--- emacs/.config/emacs/lisp/modules-lang/wsain-module-lisp.el
+++ emacs/.config/emacs/lisp/modules-lang/wsain-module-lisp.el
@@ -2,12 +2,10 @@
 (straight-use-package 'parinfer-rust-mode)
 
 (add-hook 'emacs-lisp-mode-hook (lambda()
-                                 (message "enter emacs-lisp-mode")
                                  (electric-pair-mode -1)
                                  (parinfer-rust-mode)))
 
 (add-hook 'lisp-interaction-mode-hook (lambda()
-                                       (message "enter emacs-lisp-mode")
                                        (electric-pair-mode -1)
                                        (parinfer-rust-mode)))
 
--- emacs/.config/emacs/lisp/modules-lang/wsain-module-markdown.el
+++ emacs/.config/emacs/lisp/modules-lang/wsain-module-markdown.el
@@ -1,4 +1,6 @@
 (straight-use-package 'markdown-mode)
+(add-hook 'markdown-mode-hook 'markdown-display-inline-images)
+
 (straight-use-package 'imenu-list)
 
 (provide 'wsain-module-markdown)
