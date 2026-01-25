907695bf91f5db79cf47c8974bc91bdbc153c53e
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-01-25 10:52:38 +0800

    [emacs] replace `use-package :straight t` with `straight-use-package`

--- emacs/.config/emacs/lisp/modules-basic/wsain-module-ui.el
+++ emacs/.config/emacs/lisp/modules-basic/wsain-module-ui.el
@@ -1,23 +1,22 @@
-(use-package dashboard
-  :straight t
-  :init
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-banner-logo-title "Welcome to Emacs")
-  (setq dashboard-center-content t)
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-set-navigator t)
-  (setq dashboard-set-init-info t)
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq initial-buffer-choice (lambda() (get-buffer "*dashboard*")))
-  (setq dashboard-items '((recents   . 10)
-                          (bookmarks . 5)
-                          (projects  . 10)
-                          (agenda    . 10)
-                          (registers . 5)))
-  :config
-  (dashboard-setup-startup-hook))
+(straight-use-package 'dashboard)
+(setq dashboard-startup-banner 'logo)
+(setq dashboard-banner-logo-title "Welcome to Emacs")
+(setq dashboard-center-content t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(setq dashboard-set-navigator t)
+(setq dashboard-set-init-info t)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
+(setq initial-buffer-choice (lambda() (get-buffer "*dashboard*")))
+(setq dashboard-items '((recents   . 10)
+                        (bookmarks . 5)
+                        (projects  . 10)
+                        (agenda    . 10)
+                        (registers . 5)))
+(with-eval-after-load 'dashboard-autoloads
+    (dashboard-setup-startup-hook))
+
 
 (straight-use-package 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
