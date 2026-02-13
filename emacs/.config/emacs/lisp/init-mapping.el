9ae8f8a0e2eac587e8c8b5672fc8f93df1a739fa
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-02-13 17:31:57 +0800

    [emacs] disable parinfer-rust, svg-tag-mode, add mapping

--- emacs/.config/emacs/lisp/init-mapping.el
+++ emacs/.config/emacs/lisp/init-mapping.el
@@ -4,4 +4,9 @@
 ;; (global-set-key (kbd "M-m") nil)
 (global-set-key (kbd "C-x C-z") nil)
 
+(global-set-key (kbd "C-x C-g") 'find-file-at-point)
+(global-set-key (kbd "C-c i m") 'imenu)
+
+(global-set-key (kbd "M-j") 'join-line)
+
 (provide 'init-mapping)
--- emacs/.config/emacs/lisp/modules-lang/wsain-module-lisp.el
+++ emacs/.config/emacs/lisp/modules-lang/wsain-module-lisp.el
@@ -1,13 +1,13 @@
-(setq parinfer-rust-auto-download t)
-(straight-use-package 'parinfer-rust-mode)
+;; (setq parinfer-rust-auto-download t)
+;; (straight-use-package 'parinfer-rust-mode)
 
-(add-hook 'emacs-lisp-mode-hook (lambda()
-                                 (electric-pair-mode -1)
-                                 (parinfer-rust-mode)))
-
-(add-hook 'lisp-interaction-mode-hook (lambda()
-                                       (electric-pair-mode -1)
-                                       (parinfer-rust-mode)))
+;; (add-hook 'emacs-lisp-mode-hook (lambda()
+;;                                  (electric-pair-mode -1)
+;;                                  (parinfer-rust-mode)))
+;;
+;; (add-hook 'lisp-interaction-mode-hook (lambda()
+;;                                        (electric-pair-mode -1)
+;;                                        (parinfer-rust-mode)))
 
 
 (provide 'wsain-module-lisp)
