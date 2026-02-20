8a6517c359d53f9809c4f5f4172f83c9dee65870
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-02-20 18:34:54 +0800

    [emacs] update treemacs config

--- emacs/.config/emacs/lisp/modules-basic/wsain-module-nav.el
+++ emacs/.config/emacs/lisp/modules-basic/wsain-module-nav.el
@@ -1,4 +1,16 @@
+(straight-use-package 'nerd-icons)
+
+(straight-use-package 'nerd-icons-dired)
+(with-eval-after-load 'nerd-icons-dired-autoloads
+ (setq neo-show-hidden-file t)
+ (add-hook 'dired-mode-hook 'nerd-icons-dired-mode))
+
+;; use treemacs-add-and-display-current-project-exclusively
 (straight-use-package 'treemacs)
+(with-eval-after-load 'treemacs
+  (define-key treemacs-mode-map (kbd "j") 'treemacs-next-line)
+  (define-key treemacs-mode-map (kbd "k") 'treemacs-previous-line))
+
 (straight-use-package 'zoxide)
 
 (provide 'wsain-module-nav)
--- emacs/.config/emacs/lisp/modules-lang/wsain-module-markdown.el
+++ emacs/.config/emacs/lisp/modules-lang/wsain-module-markdown.el
@@ -1,5 +1,6 @@
 (straight-use-package 'markdown-mode)
 (add-hook 'markdown-mode-hook 'markdown-display-inline-images)
+(setq markdown-max-image-size '(800 . 600))
 
 (straight-use-package 'imenu-list)
 
