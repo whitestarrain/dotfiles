f2218bbfc9b511f084097dc262b78786894bd4f3
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-02-21 16:07:17 +0800

    [emacs] update mapping

--- emacs/.config/emacs/lisp/init-mapping.el
+++ emacs/.config/emacs/lisp/init-mapping.el
@@ -9,17 +9,17 @@
 
 (global-set-key (kbd "M-j") 'join-line)
 
-(defun scroll-half-page-down ()
+(defun move-point-half-page-down ()
   "scroll down half the page"
   (interactive)
-  (scroll-down (/ (window-body-height) 2)))
+  (next-line (/ (window-body-height) 2)))
 
-(defun scroll-half-page-up ()
+(defun move-point-half-page-up ()
   "scroll up half the page"
   (interactive)
-  (scroll-up (/ (window-body-height) 2)))
+  (previous-line (/ (window-body-height) 2)))
 
-(global-set-key (kbd "M-]") 'scroll-half-page-up)
-(global-set-key (kbd "M-[") 'scroll-half-page-down)
+(global-set-key (kbd "M-]") 'move-point-half-page-down)
+(global-set-key (kbd "M-[") 'move-point-half-page-up)
 
 (provide 'init-mapping)
