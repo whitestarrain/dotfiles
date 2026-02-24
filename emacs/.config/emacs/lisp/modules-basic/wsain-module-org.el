d5d102053dc37011c26ee74cddfe84e0309d1271
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2026-02-24 08:29:06 +0800

    [emacs] add org-ql

--- emacs/.config/emacs/lisp/modules-basic/wsain-module-org.el
+++ emacs/.config/emacs/lisp/modules-basic/wsain-module-org.el
@@ -8,7 +8,7 @@
                                 (emacs-lisp . t)
                                 (python . t)))
   ;; todo (run org-mode-restart after using `#+TODO:`)
-  (setq org-startup-folded "overview")
+  (setq org-startup-folded 'fold)
   (setq org-enforce-todo-dependencies t)
   (setq org-log-into-drawer t)
   (setq org-todo-keywords
@@ -25,17 +25,17 @@
                                                       ("@work" . ?w)
                                                       (:endgrouptag))))
 
-
   ;; agenda
   (make-directory "~/Agenda" t)
   (setq org-agenda-files '("~/Agenda"))
   (setq org-default-notes-file "~/Agenda/default.org"))
 
 (straight-use-package 'org-download)
-
 (with-eval-after-load 'org-download-autoloads
     (org-download-enable)
     (setq-default org-download-image-dir "./image"))
+
+(straight-use-package 'org-ql)
 
 ;; (straight-use-package 'org-tidy)
 ;; (add-hook 'org-mode-hook #'org-tidy-mode)
