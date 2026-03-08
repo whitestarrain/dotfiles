063d2a8916e94e0018ad57fae58a8dd4921ff18d
wsain <55945266+whitestarrain@users.noreply.github.com>
2026-03-08 14:37:13 +0800

    [emacs] support refile create new headline

--- emacs/.config/emacs/lisp/modules-basic/wsain-module-ivy.el
+++ emacs/.config/emacs/lisp/modules-basic/wsain-module-ivy.el
@@ -13,5 +13,9 @@
 (setopt ivy-use-virtual-buffers t)
 (setopt enable-recursive-minibuffers t)
 
+;; makes org-refile outline working with helm/ivy
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm) ; M-i to insert ivy current item
 
 (provide 'wsain-module-ivy)
