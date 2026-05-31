fda057107c35bd019dad83def6400d621b8758f3
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2026-05-31 21:58:00 +0800

    [nvim] close md-summary-toc before saving session

--- nvim/.config/nvim/lua/wsain/plugin/configs/extra/md-summary-toc.lua
+++ nvim/.config/nvim/lua/wsain/plugin/configs/extra/md-summary-toc.lua
@@ -4,6 +4,7 @@ plugin.short_url = "whitestarrain/md-summary-toc.nvim"
 plugin.load_event = "VeryLazy"
 plugin.config = function()
   require("md-summary-toc").setup()
+  require("wsain.utils").add_command_before_save_session("silent! MdSummaryTocClose")
 end
 
 return plugin
