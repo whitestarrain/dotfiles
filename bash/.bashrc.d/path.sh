ea8c0cfd6cb9255bfb18ac72f5f9d94267c8f58c
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2026-06-30 08:31:16 +0800

    [bash] add cargo bin path

--- bash/.bashrc.d/path.sh
+++ bash/.bashrc.d/path.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+add_to_path() {
+    local dir re
+
+    for dir; do # for dir in "$@"; do
+        re="(^$dir:|:$dir:|:$dir$)"
+        if ! [[ $PATH =~ $re ]]; then
+            PATH="$dir:$PATH"
+        fi
+    done
+}
+
 # add path
-PATH=$PATH:~/go/bin
-PATH=$PATH:~/.bin
+add_to_path ~/.bin
+add_to_path ~/go/bin
+add_to_path ~/.local/share/cargo/bin
 
